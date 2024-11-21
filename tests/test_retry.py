dd3f3578b9d88f30d2d98665bb03234624a579ac
Lev Vereshchagin <mail@vrslev.com>
2024-11-21 12:30:18 +0300

    Update

--- tests/test_retry.py
+++ tests/test_retry.py
@@ -5,7 +5,7 @@ import stamina
 from any_llm_client.retry import RequestRetryConfig
 
 
-def test_default_kwargs_match() -> None:
+def test_request_retry_config_default_kwargs_match() -> None:
     assert (
         inspect.getfullargspec(RequestRetryConfig).kwonlydefaults
         == inspect.getfullargspec(stamina.retry).kwonlydefaults
