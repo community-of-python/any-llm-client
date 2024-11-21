c0f1c2e82a8bcf53813842e541631e5387300086
Lev Vereshchagin <mail@vrslev.com>
2024-11-21 12:40:54 +0300

    Fix formatting

--- any_llm_client/__init__.py
+++ any_llm_client/__init__.py
@@ -1,7 +1,7 @@
-from any_llm_client.core import LLMClient, LLMConfig, LLMError, Message, MessageRole, OutOfTokensOrSymbolsError
 from any_llm_client.clients.mock import MockLLMClient, MockLLMConfig
 from any_llm_client.clients.openai import OpenAIClient, OpenAIConfig
 from any_llm_client.clients.yandexgpt import YandexGPTClient, YandexGPTConfig
+from any_llm_client.core import LLMClient, LLMConfig, LLMError, Message, MessageRole, OutOfTokensOrSymbolsError
 from any_llm_client.main import AnyLLMConfig, get_client
 
 
--- any_llm_client/main.py
+++ any_llm_client/main.py
@@ -3,10 +3,10 @@ import typing
 
 import httpx
 
-from any_llm_client.core import LLMClient
 from any_llm_client.clients.mock import MockLLMClient, MockLLMConfig
 from any_llm_client.clients.openai import OpenAIClient, OpenAIConfig
 from any_llm_client.clients.yandexgpt import YandexGPTClient, YandexGPTConfig
+from any_llm_client.core import LLMClient
 from any_llm_client.retry import RequestRetryConfig
 
 
