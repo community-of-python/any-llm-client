f8bcd395cd8c9a0c1a2903815134c4d12d62883c
Lev Vereshchagin <mail@vrslev.com>
2024-11-22 15:31:09 +0300

    Fix http integration

--- any_llm_client/http.py
+++ any_llm_client/http.py
@@ -14,14 +14,14 @@ DEFAULT_HTTP_TIMEOUT: typing.Final = urllib3.Timeout(total=None, connect=5.0)
 
 
 def get_http_client_from_kwargs(kwargs: dict[str, typing.Any]) -> niquests.AsyncSession:
-    kwargs_with_defaults: typing.Final = kwargs.copy()
-    kwargs_with_defaults.setdefault("timeout", DEFAULT_HTTP_TIMEOUT)
+    modified_kwargs: typing.Final = kwargs.copy()
+    timeout: typing.Final = modified_kwargs.pop("timeout", DEFAULT_HTTP_TIMEOUT)
+    proxies: typing.Final = modified_kwargs.pop("proxies", None)
 
-    timeout: typing.Final = kwargs_with_defaults.pop("timeout")
-    session: typing.Final = niquests.AsyncSession(**kwargs_with_defaults)
-    if proxies := kwargs.get("proxies"):
+    session: typing.Final = niquests.AsyncSession(**modified_kwargs)
+    session.custom_timeout = timeout  # type: ignore[attr-defined]
+    if proxies:
         session.proxies = proxies
-    session.timeout = timeout
     return session
 
 
@@ -33,7 +33,7 @@ async def make_http_request(
 ) -> niquests.Response:
     @stamina.retry(on=niquests.HTTPError, **dataclasses.asdict(request_retry))
     async def make_request_with_retries() -> niquests.Response:
-        response: typing.Final = await httpx_client.send(build_request(), timeout=httpx_client.timeout)
+        response: typing.Final = await httpx_client.send(build_request(), timeout=httpx_client.custom_timeout)  # type: ignore[attr-defined]
         response.raise_for_status()
         return response
 
@@ -49,7 +49,11 @@ async def make_streaming_http_request(
 ) -> typing.AsyncIterator[niquests.AsyncResponse]:
     @stamina.retry(on=httpx.HTTPError, **dataclasses.asdict(request_retry))
     async def make_request_with_retries() -> niquests.AsyncResponse:
-        response: typing.Final = await httpx_client.send(build_request(), stream=True, timeout=httpx_client.timeout)
+        response: typing.Final = await httpx_client.send(
+            build_request(),
+            stream=True,
+            timeout=httpx_client.custom_timeout,  # type: ignore[attr-defined]
+        )
         response.raise_for_status()
         return response  # type: ignore[return-value]
 
--- tests/test_http.py
+++ tests/test_http.py
@@ -8,20 +8,30 @@ from any_llm_client.http import DEFAULT_HTTP_TIMEOUT, get_http_client_from_kwarg
 
 class TestGetHttpClientFromKwargs:
     def test_http_timeout_is_added(self) -> None:
-        original_kwargs: typing.Final = {"mounts": {"http://": None}}
+        original_kwargs: typing.Final = {"resolver": None}
         passed_kwargs: typing.Final = copy.deepcopy(original_kwargs)
 
         client: typing.Final = get_http_client_from_kwargs(passed_kwargs)
 
-        assert client.timeout == DEFAULT_HTTP_TIMEOUT
+        assert client.custom_timeout == DEFAULT_HTTP_TIMEOUT  # type: ignore[attr-defined]
         assert original_kwargs == passed_kwargs
 
     def test_http_timeout_is_not_modified_if_set(self) -> None:
         timeout: typing.Final = httpx.Timeout(7, connect=5, read=3)
-        original_kwargs: typing.Final = {"mounts": {"http://": None}, "timeout": timeout}
+        original_kwargs: typing.Final = {"resolver": None, "timeout": timeout}
         passed_kwargs: typing.Final = copy.deepcopy(original_kwargs)
 
         client: typing.Final = get_http_client_from_kwargs(passed_kwargs)
 
-        assert client.timeout == timeout
+        assert client.custom_timeout == timeout  # type: ignore[attr-defined]
+        assert original_kwargs == passed_kwargs
+
+    def test_http_proxies_are_added(self) -> None:
+        proxies: typing.Final = {"http": "http://example"}
+        original_kwargs: typing.Final = {"resolver": None, "proxies": proxies}
+        passed_kwargs: typing.Final = copy.deepcopy(original_kwargs)
+
+        client: typing.Final = get_http_client_from_kwargs(passed_kwargs)
+
+        assert client.proxies == proxies
         assert original_kwargs == passed_kwargs
